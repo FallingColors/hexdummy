0534696a56f9847ca3c81b65606d0e864a0c4cbb
object-Object <object@objectobject.ca>
2025-03-06 19:37:41 -0500

    Switch to dev.panuszewski.typesafe-conventions

--- template/buildSrc/build.gradle.kts
+++ template/buildSrc/build.gradle.kts
@@ -1,6 +1,4 @@
-import org.gradle.api.internal.artifacts.DefaultModuleIdentifier
-import org.gradle.api.internal.artifacts.dependencies.DefaultMinimalDependency
-import org.gradle.api.internal.artifacts.dependencies.DefaultMutableVersionConstraint
+import dev.panuszewski.gradle.pluginMarker
 
 plugins {
     `kotlin-dsl`
@@ -16,18 +14,13 @@ repositories {
 }
 
 dependencies {
-    // plugins
-    // https://github.com/gradle/gradle/issues/15383#issuecomment-1855984127
-    implementation(libs.plugins.kotlin.jvm.toLibrary())
-    implementation(libs.plugins.architectury.toLibrary())
-    implementation(libs.plugins.architectury.loom.toLibrary())
-    implementation(libs.plugins.shadow.toLibrary())
-    implementation(libs.plugins.modPublish.toLibrary())
-    implementation(libs.plugins.pkJson5.toLibrary())
-
-    // https://stackoverflow.com/a/70878181
-    // https://github.com/gradle/gradle/issues/15383
-    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+    // plugins used in convention plugins
+    implementation(pluginMarker(libs.plugins.kotlin.jvm))
+    implementation(pluginMarker(libs.plugins.architectury.asProvider()))
+    implementation(pluginMarker(libs.plugins.architectury.loom))
+    implementation(pluginMarker(libs.plugins.shadow))
+    implementation(pluginMarker(libs.plugins.modPublish))
+    implementation(pluginMarker(libs.plugins.pkJson5))
 }
 
 java {
@@ -35,14 +28,3 @@ java {
         languageVersion = JavaLanguageVersion.of(libs.versions.java.get())
     }
 }
-
-// support declaring dependencies on libs.plugins.*
-
-fun ProviderConvertible<PluginDependency>.toLibrary() = asProvider().toLibrary()
-
-fun Provider<PluginDependency>.toLibrary() = get().toLibrary()
-
-fun PluginDependency.toLibrary() = DefaultMinimalDependency(
-    DefaultModuleIdentifier.newId(pluginId, "$pluginId.gradle.plugin"),
-    DefaultMutableVersionConstraint(version),
-)
--- template/buildSrc/settings.gradle.kts
+++ template/buildSrc/settings.gradle.kts
@@ -1,8 +1,5 @@
-dependencyResolutionManagement {
-    versionCatalogs {
-        // allow referencing the version catalog from the main project
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
+plugins {
+    // allows versions from gradle/libs.versions.toml to be used in buildSrc
+    // see: https://github.com/gradle/gradle/issues/15383
+    id("dev.panuszewski.typesafe-conventions") version "0.4.1"
 }
